a3e18804af023f4e54c63a8c99c9f89376e0c5ee
Kashif <kashif.riaz7272@gmail.com>
2024-10-04 17:52:40 +0500

    added tailwind files

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        custom: ["Inter", "sans-serif"],
+      },
+      colors: {
+        customblack: "#111928",
+        custompurple: "#7E3AF2",
+        customgrey: "#6B7280",
+      },
+    },
+  },
+  plugins: [],
+};
